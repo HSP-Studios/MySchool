af69561059b4bede549b70cdb44010785dbac454
kevin909867 <169435465+HighSchoolProgrammer@users.noreply.github.com>
2025-10-21 21:02:51 +1000

    Add Velopack auto-update support to application
    Integrated Velopack for application updates by adding update logic and main entry point in App.xaml.cs. Updated project file to include Velopack package, set StartupObject, and adjusted App.xaml handling for correct startup configuration.

--- MySchool/App.xaml.cs
+++ MySchool/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using MySchool.Classes;
 using System;
+using System.Configuration;
+using System.Data;
 using System.IO;
 using System.Text.Json;
-using MySchool.Classes;
+using System.Windows;
+using Velopack;
+using Velopack.Sources;
 
 namespace MySchool
 {
@@ -14,6 +16,32 @@ namespace MySchool
     public partial class App : Application
     {
         public static UserSettings CurrentSettings { get; private set; } = new UserSettings();
+
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            VelopackApp.Build().Run();
+            App app = new();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        private static async Task UpdateApp()
+        {
+            // https://docs.velopack.io/reference/cs/Velopack/Sources/GithubSource/constructors
+            var mgr = new UpdateManager(new GithubSource("https://github.com/HSP-Studios/MySchool", null, false, null));
+
+            // check for new version
+            var newVersion = await mgr.CheckForUpdatesAsync();
+            if (newVersion == null)
+                return; // no update available
+
+            // download new version
+            await mgr.DownloadUpdatesAsync(newVersion);
+
+            // install new version and restart app
+            mgr.ApplyUpdatesAndRestart(newVersion);
+        }
 
         protected override void OnStartup(StartupEventArgs e)
         {
@@ -38,5 +66,4 @@ namespace MySchool
             }
         }
     }
-
 }
